b96e234912c7d44beed315b01c2873dd8d356ef9
Michele Sciabarra <michele@nuvolaris.io>
2023-04-16 22:43:26 +0100

    fixed link in linux

--- 1-deploy.sh
+++ 1-deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-VER=0.3.0-morpheus.23041217
+VER=0.3.0-morpheus.23041622
 URL="https://github.com/nuvolaris/nuv/releases/download/$VER/nuv_${VER}_amd64.deb"
 wget --no-verbose $URL -O nuv.deb
 sudo dpkg -i nuv.deb
-sudo ln -sf /usr/nuvolaris/nuv /usr/local/bin/nuv
 nuv -update
 nuv -info
 nuv setup --devcluster
