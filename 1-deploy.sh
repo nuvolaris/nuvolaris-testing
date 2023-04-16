94aec28cac47fc6627671fef272d5c45009f9fbe
Michele Sciabarra <michele@nuvolaris.io>
2023-04-16 17:31:26 +0100

    kind-1681662686

--- 1-deploy.sh
+++ 1-deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 VER=0.3.0-morpheus.23041217
 URL="https://github.com/nuvolaris/nuv/releases/download/$VER/nuv_${VER}_amd64.deb"
-wget $URL -O nuv.deb
-dpkg -i nuv.deb
+wget --no-verbose $URL -O nuv.deb
+sudo dpkg -i nuv.deb
+alias nuv=/usr/nuvolaris/nuv
 nuv -update
 nuv -info
+nuv setup --devcluster
+nuv -wsk action create hello hello.js
+nuv -wsk action invoke hello -p name Nuvolaris -r | tee output.txt
+if grep Nuvolaris output.txt
+then echo SUCCESS ; exit 0
+else echo FAIL ; exit 1
+fi
