289089bb4bb0e3d71f8687ba9246e4fe4e5778f4
Michele Sciabarra <michele@sciabarra.com>
2023-04-16 17:11:42 +0100

    kind-1681661502

--- 1-deploy.sh
+++ 1-deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "TODO!"
+VER=0.3.0-morpheus.23041217
+URL="https://github.com/nuvolaris/nuv/releases/download/$VER/nuv_${VER}_amd64.deb"
+wget $URL -O nuv.deb
+dpkg -i nuv.deb
+nuv -update
+nuv -info
