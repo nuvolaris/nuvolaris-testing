4f09f9c49f278781c351d3cfa55a03a10815518e
Michele Sciabarra <michele@nuvolaris.io>
2023-05-15 07:34:06 +0100

    changed dns name for vm

--- lib/createAwsVm.sh
+++ lib/createAwsVm.sh
@@ -20,7 +20,7 @@ TYPE=${1:?type}
 TYPE="$(echo $TYPE | awk -F- '{print $1}')"
 STACK=nuvolaris-testing-$TYPE
 CONF=$TYPE.cf
-HOST=$TYPE-nuv-test
+HOST=$TYPE-nuv-test2
 DNS=$HOST.duckdns.org
 
 # create vm
