8894aba7d035fdd3048cd097307d13ed73d702c1
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-07-10 18:20:56 +0200

    feat: add user login test with kind

--- 6-login.sh
+++ 6-login.sh
@@ -15,4 +15,35 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-echo "TODO!"
+TYPE="${1:?test type}"
+TYPE="$(echo $TYPE | awk -F- '{print $1}')"
+
+# Generate a random password for the user "demo"
+password=$(nuv -random --str 12)
+
+# Create a new user "demo-user" with nuv admin adduser with previous services enabled
+if nuv admin adduser demo-user demo@email.com $password --redis --mongodb --minio | grep "whiskuser.nuvolaris.org/demo-user created"
+then echo SUCCESS CREATING demo-user
+else echo FAIL CREATING demo-user ; exit 1 
+fi
+
+sleep 10
+
+case "$TYPE" in
+    (kind) 
+        if NUV_LOGIN="demo-user" NUV_PASSWORD=$password nuv -login http://localhost:3233 | grep "Successfully logged in as demo-user."
+        then echo SUCCESS LOGIN
+        else echo FAIL LOGIN ; exit 1 
+        fi
+    ;;
+esac
+
+if nuv setup nuvolaris hello | grep hello
+then echo SUCCESS ; exit 0
+else echo FAIL ; exit 1 
+fi
+
+if nuv -wsk action list | grep /demo-user/hello/hello
+then echo SUCCESS ; exit 0
+else echo FAIL ; exit 1 
+fi
