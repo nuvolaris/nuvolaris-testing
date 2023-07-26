b8f649afad1f65f302269e7909a2542d933727c7
Francesco Timperi Tiberi <francesco.timperi@gmail.com>
2023-07-27 00:21:06 +0100

    fix: various fixes in user test and added user postgres test

--- 15-user-postgres.sh
+++ 15-user-postgres.sh
@@ -18,15 +18,15 @@
 TYPE="${1:?test type}"
 TYPE="$(echo $TYPE | awk -F- '{print $1}')"
 
-if nuv config status | grep NUVOLARIS_MINIO=true 
-then echo "MINIO ENABLED"
-else echo "MINIO DISABLED - SKIPPING" ; exit 0
+if nuv config status | grep NUVOLARIS_POSTGRES=true 
+then echo "POSTGRES ENABLED"
+else echo "POSTGRES DISABLED - SKIPPING" ; exit 0
 fi
 
-user="demominiouser"
+user="demopostgresuser"
 password=$(nuv -random --str 12)
 
-if nuv admin adduser $user $user@email.com $password --minio | grep "whiskuser.nuvolaris.org/$user created"
+if nuv admin adduser $user $user@email.com $password --postgres | grep "whiskuser.nuvolaris.org/$user created"
 then echo SUCCESS CREATING $user
 else echo FAIL CREATING $user; exit 1 
 fi
@@ -41,7 +41,7 @@ case "$TYPE" in
         fi
     ;;
     *)
-        APIURL=$(nuv debug apihost | awk '/whisk API host/{print $4}')
+        APIURL=$(nuv debug apihost | awk '/whisk API host/{print $4}')        
         if NUV_LOGIN=$user NUV_PASSWORD=$password nuv -login $APIURL | grep "Successfully logged in as $user."
         then echo SUCCESS LOGIN
         else echo FAIL LOGIN ; exit 1 
@@ -49,33 +49,24 @@ case "$TYPE" in
     ;;    
 esac
 
-if nuv setup nuvolaris minio | grep hello
-then echo SUCCESS SETUP MINIO ACTION
-else echo FAIL ; exit 1 
+if nuv setup nuvolaris postgres | grep 'Nuvolaris Postgres is up and running!'
+then echo SUCCESS SETUP POSTGRES ACTION
+else echo FAIL SETUP POSTGRES ACTION; exit 1 
 fi
 
-if nuv -wsk action list | grep "/$user/hello/minio"
-then echo SUCCESS USER MINIO ACTION LIST
-else echo FAIL ; exit 1 
+if nuv -wsk action list | grep "/$user/hello/postgres"
+then echo SUCCESS USER POSTGRES ACTION LIST
+else echo FAIL USER POSTGRES ACTION LIST; exit 1 
 fi
 
-MINIO_ACCESS_KEY=$(nuv -config MINIO_ACCESS_KEY)
-MINIO_SECRET_KEY=$(nuv -config MINIO_SECRET_KEY)
-MINIO_HOST=$(nuv -config MINIO_HOST)
-MINIO_PORT=$(nuv -config MINIO_PORT)
-MINIO_DATA_BUCKET=$(nuv -config MINIO_DATA_BUCKET)
-MINIO_STATIC_BUCKET=$(nuv -config MINIO_STATIC_BUCKET)
 
-if [[ -z "$MINIO_ACCESS_KEY" ]]
-then echo FAIL USER MINIO_ACCESS_KEY; exit 1
-else echo SUCCESS USER MINIO_ACCESS_KEY
+POSTGRES_URL=$(nuv -config POSTGRES_URL)
+if [ -z "$POSTGRES_URL" ]
+then echo FAIL USER POSTGRES_URL; exit 1
+else echo SUCCESS USER POSTGRES_URL
 fi 
 
-if nuv -wsk action invoke /$user/hello/minio -p minio_access "$MINIO_ACCESS_KEY" \
-      -p minio_secret "$MINIO_SECRET_KEY" \
-      -p minio_host "$MINIO_HOST" \
-      -p minio_port "$MINIO_PORT" \
-      -p minio_data "$MINIO_DATA_BUCKET" -r| grep "$user-data"
+if nuv -wsk action invoke /$user/hello/postgres -p dburi "$POSTGRES_URL" -r| grep 'Nuvolaris Postgres is up and running!'
 then echo SUCCESS ; exit 0
 else echo FAIL ; exit 1 
 fi
