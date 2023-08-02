0f3141761563d586ebbb10813ab9a36f6c02cb78
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-08-02 18:55:07 +0200

    fix: deploy aks

--- 1-deploy.sh
+++ 1-deploy.sh
@@ -20,72 +20,78 @@ TYPE="$(echo $TYPE | awk -F- '{print $1}')"
 
 # actual setup
 case "$TYPE" in
-    (kind)
-        # create vm with docker
-        nuv config reset
-        nuv setup devcluster --uninstall
-        nuv setup devcluster
+kind)
+    # create vm with docker
+    nuv config reset
+    nuv setup devcluster --uninstall
+    nuv setup devcluster
     ;;
-    (k3s)
-        # create vm and install in the server
-        nuv config reset
-        task aws:config
-        # create vm without k3s
-        nuv cloud aws vm-create k3s-test
-        nuv cloud aws zone-update k3s.n9s.cc --wildcard --vm=k3s-test
-        nuv cloud aws vm-getip k3s-test >_ip
-        # install nuvolaris
-        nuv setup server $(cat _ip) ubuntu --uninstall
-        nuv setup server $(cat _ip) ubuntu
+k3s)
+    # create vm and install in the server
+    nuv config reset
+    task aws:config
+    # create vm without k3s
+    nuv cloud aws vm-create k3s-test
+    nuv cloud aws zone-update k3s.n9s.cc --wildcard --vm=k3s-test
+    nuv cloud aws vm-getip k3s-test >_ip
+    # install nuvolaris
+    nuv setup server $(cat _ip) ubuntu --uninstall
+    nuv setup server $(cat _ip) ubuntu
     ;;
-    (mk8s)
-        nuv config reset
-        task aws:config
-        # create vm with mk8s
-        nuv cloud aws vm-create mk8s-test
-        nuv cloud aws zone-update mk8s.n9s.cc --wildcard --vm=mk8s-test
-        nuv cloud aws vm-getip mk8s-test >_ip
-        nuv cloud mk8s create SERVER=$(cat _ip) USERNAME=ubuntu
-        nuv cloud mk8s kubeconfig SERVER=$(cat _ip) USERNAME=ubuntu
-        # install cluster
-        nuv setup cluster --uninstall
-        nuv setup cluster
+mk8s)
+    nuv config reset
+    task aws:config
+    # create vm with mk8s
+    nuv cloud aws vm-create mk8s-test
+    nuv cloud aws zone-update mk8s.n9s.cc --wildcard --vm=mk8s-test
+    nuv cloud aws vm-getip mk8s-test >_ip
+    nuv cloud mk8s create SERVER=$(cat _ip) USERNAME=ubuntu
+    nuv cloud mk8s kubeconfig SERVER=$(cat _ip) USERNAME=ubuntu
+    # install cluster
+    nuv setup cluster --uninstall
+    nuv setup cluster
     ;;
-    (eks)
-        nuv config reset
-        # create cluster
-        task aws:config
-        task eks:config
-        nuv cloud eks create
-        nuv cloud eks kubeconfig
-        nuv cloud eks lb >_cname
-        nuv cloud aws zone-update eks.n9s.cc --wildcard --cname=$(cat _cname)
-        # on eks we need to setup an initial apihost resolving the NLB hostname
-        nuv config apihost nuvolaris.eks.n9s.cc
-        # install cluster
-        nuv setup cluster --uninstall
-        nuv setup cluster
+eks)
+    nuv config reset
+    # create cluster
+    task aws:config
+    task eks:config
+    nuv cloud eks create
+    nuv cloud eks kubeconfig
+    nuv cloud eks lb >_cname
+    nuv cloud aws zone-update eks.n9s.cc --wildcard --cname=$(cat _cname)
+    # on eks we need to setup an initial apihost resolving the NLB hostname
+    nuv config apihost nuvolaris.eks.n9s.cc
+    # install cluster
+    nuv setup cluster --uninstall
+    nuv setup cluster
     ;;
-    (aks)
-        # create cluster
-        task aks:config
-        nuv cloud aks create
-        nuv cloud aks kubeconfig
-        # install cluster
-        nuv setup cluster --uninstall
-        nuv setup cluster
-    ;;
-    (gke)
-        # create cluster
-        task gke:config
-        nuv cloud gke create
-        nuv cloud gke kubeconfig
+aks)
+    # create cluster
+    task aks:config
+    nuv cloud aks create
+    nuv cloud aks kubeconfig
 
-        task aws:config        
-        IP=$(nuv cloud gke lb)
-        nuv cloud aws zone-update gke.n9s.cc --wildcard --ip $IP
-        
-        # install cluster
-        nuv setup cluster --uninstall
-        nuv setup cluster
+    task aws:config
+    IP=$(nuv cloud aks lb)
+    nuv cloud aws zone-update aks.n9s.cc --wildcard --ip $IP
+
+    # install cluster
+    nuv setup cluster --uninstall
+    nuv setup cluster
+    ;;
+gke)
+    # create cluster
+    task gke:config
+    nuv cloud gke create
+    nuv cloud gke kubeconfig
+
+    task aws:config
+    IP=$(nuv cloud gke lb)
+    nuv cloud aws zone-update gke.n9s.cc --wildcard --ip $IP
+
+    # install cluster
+    nuv setup cluster --uninstall
+    nuv setup cluster
+    ;;
 esac
--- README.md
+++ README.md
@@ -6,23 +6,23 @@ The goal of the challenge is to pass all the tests in the grid, using GitHub Act
 
 # Nuvolaris 0.3 "Morpheus" Testing
 
-## Acceptance Test Status: 59/96
+## Acceptance Test Status: 70/96
 <img src="img/progress.svg" width="63%">
 
 |  |               |Kind|M8S |K3S |EKS |AKS |GKE |OSH |
 |--|---------------|----|----|----|----|----|----|----|
 |1 |Deploy         | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
 |2 |SSL            | N/A| ✅ | ✅ | ✅ | ✅ | ✅ | ❌ |
-|3 |Sys Redis      | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ |
-|4a|Sys FerretDB   | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ | 
-|4b|Sys Postgres   | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ | 
-|5 |Sys Minio      | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ | 
-|6 |Login          | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ | 
-|7 |Statics        | N/A| ✅ | ✅ | ✅ | ❌ | ✅ | ❌ | 
-|8 |User Redis     | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ | 
-|9a|User FerretDB  | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ |
-|9b|User Postgres  | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ | 
-|10|User Minio     | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ❌ | 
+|3 |Sys Redis      | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ |
+|4a|Sys FerretDB   | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
+|4b|Sys Postgres   | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
+|5 |Sys Minio      | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
+|6 |Login          | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
+|7 |Statics        | N/A| ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
+|8 |User Redis     | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
+|9a|User FerretDB  | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ |
+|9b|User Postgres  | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
+|10|User Minio     | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | 
 |11|Nuv Win        | ❌ | ❌ | ❌ | ❌ | ❌ | ❌ | ❌ |
 |12|Nuv Mac        | ❌ | ❌ | ❌ | ❌ | ❌ | ❌ | ❌ |  
 |--|---------------|----|----|----|----|----|----|----|
